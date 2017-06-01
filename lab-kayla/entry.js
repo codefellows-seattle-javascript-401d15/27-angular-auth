0535c77b6efb17c74d89fef232f6997a12666e0d
Kayla Asay <kayla-asay@Ubuntu16.04-01>
2017-05-31 17:19:32 -0700

    class

--- lab-kayla/entry.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-context.keys().forEach(key => {
-  let name = camelcase(path.basename(key, '.js'))
-  let module = context(key)
-  routesApp.service(name, module)
-})
-
-context = require.context('./component/', true, )
