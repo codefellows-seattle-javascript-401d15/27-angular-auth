25fe027a333c31ed7d263e2d550be9587b96a769
Brianna <briannaburrows@gmail.com>
2017-06-01 11:51:09 -0700

    revised router-config with demo code

--- lab-brianna/app/config/router-config.js
+++ lab-brianna/app/config/router-config.js
@@ -1,46 +1,29 @@
 'use strict'
 
-module.exports = [
-  '$stateProvider',
-  '$urlRouterProvider',
-  routerConfig]
+module.exports = ['$stateProvider', '$urlRouterProvider', routerConfig]
 
-  function routerConfig($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.when('', '/home')
-    $urlRouterProvider.when('/', '/join#signup')
-    $urlRouterProvider.when('/signup', '/join#signup')
-    $urlRouterProvider.when('/login', '/join#login')
+function routerConfig($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.when('', '/home')
+  $urlRouterProvider.when('/', '/join#signup')
+  $urlRouterProvider.when('/signup', '/join#signup')
+  $urlRouterProvider.when('/login', '/join#login')
 
-    let routes = [
-      {
-        name: 'home',
-        url: '/home',
-        template: require('../view/home/home.html'),
-        controller: 'HomeController',
-        controllerAs: 'homeCtrl'
-      },
-      {
-        name: 'landing',
-        url: '/join',
-        template: require('../view/landing/landing.html'),
-        controller: 'LandingController',
-        controllerAs: 'landingCtrl'
-      }
-      // {
-      //   name: 'login',
-      //   url: '/login',
-      //   template: require('../component/landing/login/login.html'),
-      //   controller: 'LoginController',
-      //   controllerAs: 'loginCtrl'
-      // },
-      // {
-      //   name: 'gallery',
-      //   url: '/gallery',
-      //   template: require('../view/gallery/gallery.html'),
-      //   controller: 'GalleryController',
-      //   controllerAs: 'galleryCtrl'
-      // }
-    ]
+  let routes = [
+    {
+      name: 'home',
+      url: '/home',
+      template: require('../view/home/home.html'),
+      controller: 'HomeController',
+      controllerAs: 'homeCtrl'
+    },
+    {
+      name: 'landing',
+      url: '/join',
+      template: require('../view/landing/landing.html'),
+      controller: 'LandingController',
+      controllerAs: 'landingCtrl'
+    }
+  ]
 
-    routes.forEach(route => $stateProvider.state(route))
-  }
+  routes.forEach($stateProvider.state)
+}
