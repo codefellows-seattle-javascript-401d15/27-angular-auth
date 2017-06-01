f9ec452608fed69355791acf6a88d73254b39c9f
Kayla Asay <kayla-asay@Ubuntu16.04-01>
2017-06-01 10:35:10 -0700

    landing

--- lab-kayla/app/config/router-config.js
+++ lab-kayla/app/config/router-config.js
@@ -1,31 +1,28 @@
 'use strict'
 
-module.exports = [
-  '$stateProvider',
-  '$urlRouterProvider',
-  $urlRouterProvider.when('', '/join#signup'),
-  $urlRouterProvider.when('/', '/join#signup'),
-  $urlRouterProvider.when('/signup', '/join#signup'),
-  $urlRouterProvider.when('/login', '/join#login'),
+module.exports = ['$stateProvider','$urlRouterProvider', routerConfig]
 
-  function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.when('', '/home')
-    let routes = [
-      {
-        name: 'home',
-        url: '/home',
-        template: require('../view/home/home.html'),
-        controller: 'HomeContriller',
-        controllerAs: 'homeCtrl'
-      },
-      {
-        name: 'landing',
-        url: '/joinlandingcontroller', //maybe??
-        template: require('../view/landing/landing.html'),
-        controller: 'LandingController',
-        controllerAs: 'landingCtrl'
-      }
-    ]
-    routes.forEach(route => $stateProvider.state(route))
-  }
-]
+function routerConfig($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.when('', '/join#signup')
+  $urlRouterProvider.when('/', '/join#signup')
+  $urlRouterProvider.when('/signup', '/join#signup')
+  $urlRouterProvider.when('/login', '/join#login')
+
+  let routes = [
+    {
+      name: 'home',
+      url: '/home',
+      template: require('../view/home/home.html'),
+      controller: 'HomeContriller',
+      controllerAs: 'homeCtrl'
+    },
+    {
+      name: 'landing',
+      url: '/join',
+      template: require('../view/landing/landing.html'),
+      controller: 'LandingController',
+      controllerAs: 'landingCtrl'
+    }
+  ]
+  routes.forEach($stateProvider.state)
+}
