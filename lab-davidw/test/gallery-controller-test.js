2becd9c127e1f2d4294e8207cc84b2bd45418d80
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-06-04 23:15:02 -0700

    add tests, but still having the maximum call stack error

---
+++ lab-davidw/test/gallery-controller-test.js
@@ -0,0 +1,28 @@
+// 'use strict';
+//
+// const angular = require('angular');
+// require('angular-mocks');
+//
+// describe('testing GalleryController', function() {
+//   beforeEach(() => {
+//     angular.mock.module('cfGramApp');
+//     angular.mock.inject(($rootScope, $controller) => {
+//       this.$rootScope = $rootScope;
+//       this.homeCtrl = new $controller('GalleryController');
+//     });
+//   });
+//
+//   afterEach(() => this.$rootScope.$apply());
+//
+//   it('should exist', () => {
+//
+//     expect(this.galleryCtrl).toBeDefined();
+//   });
+//
+//   it('should have a title', () => {
+//
+//     expect(this.galleryCtrl.title).toBeDefined();
+//     expect(this.galleryCtrl.title).toEqual(jasmine.any(String));
+//     expect(this.galleryCtrl.title).toBe('Welcome to Gallery');
+//   });
+// });
