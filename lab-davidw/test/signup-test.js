159c1ed35d2a638bd11004419a0ee11531eebae6
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-06-04 19:23:55 -0700

    add my best attempt at matching the mocks

---
+++ lab-davidw/test/signup-test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//re-watch last hour of 6/1 vid
+
+// require('angular');
+// require('angular-mocks');
+//
+// describe('testing signup' function(){
+//   beforeEach(() => {
+//     angular.mock.module('cfgram')
+//   })
+// })
